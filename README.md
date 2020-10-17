4d727f835032d74724cdc537a8b01a3af7adf8ca
Victor Demilly <vicdemilly@gmail.com>
2020-10-17 16:58:59 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# vue-setup
+ Setup Laravel Mix
---
+++ src/components/Example.vue
@@ -0,0 +1,11 @@
+<template>
+    <div>
+        <p>this is a test</p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: "Example"
+    }
+</script>
