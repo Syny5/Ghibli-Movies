19e0f1c9135fe51485d2b8e0f9b4594583be0dfc
Victor Demilly <vicdemilly@gmail.com>
2020-10-19 17:57:00 +0200

    Initial commit

--- src/app.js
+++ src/app.js
@@ -1,10 +1,34 @@
 import Vue from 'vue';
-import Example from './components/Example.vue';
+
+window.axios = require('axios');
+
+import Ghibli from './components/Ghibli.vue';
 
 new Vue({
     el: '#app',
 
     components: {
-        Example,
+        Ghibli,
+    },
+
+    mounted: function(){
+        axios.get('https://ghibliapi.herokuapp.com/films')
+            .then(response => this.posts = response.data)
+            .catch(error => this.posts = [{title: 'No posts found.'}]);
+    },
+
+    data: function(){
+        return{
+            posts: null,
+            like: false
+        }
+    },
+
+    computed: {
+        likedClass: function(){
+            return { 
+                'likedMovie' : this.like 
+            }
+        }
     }
 });
---
+++ src/components/Ghibli.vue
@@ -0,0 +1,26 @@
+<template>
+    <div class="movieCards" :class="likedClass" @click="like = !like">
+        <h2 v-text="post.title"></h2><span class="date">({{ post.release_date }})</span>
+        <p class="producerDirector">Produced by {{ post.producer }} and directed by {{ post.director }}</p>
+        <p v-text="post.description" class="description"></p>
+    </div>
+</template>
+
+<script>
+    export default{
+        name: 'Ghibli',
+        props: ['post'],
+        data: function(){
+            return{
+                like: false
+            }
+        },
+        computed: {
+            likedClass: function(){
+                return { 
+                    'likedMovie' : this.like 
+                }
+            }
+        }
+    }
+</script>
